8e8cf118424ae0800d63985f85e9fc7be086cc79
ale2002solano <ale134valle@gmail.com>
2024-11-28 16:04:51 -0600

    vamos de bounce a shake

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -135,7 +135,7 @@ export default function Navbar() {
           <BounceProvider>
             <button onClick={handleCarritoClick} title="carrito" className="relative">
               <FaShoppingCart className="text-gray-700 text-3xl" />
-              <span className={`absolute top-0 right-0 w-3.5 h-3.5 bg-purple-500 rounded-full border-2 border-none ${isBounce ? 'animate-bounce' : ''}`}></span>
+              <span className={`absolute top-0 right-0 w-3.5 h-3.5 bg-purple-500 rounded-full border-2 border-none ${isBounce ? 'animate-shake' : ''}`}></span>
             </button>
           </BounceProvider>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,17 +18,25 @@ const config: Config = {
         koulen: ['var(--koulen)'],
         inter: ['var(--inter)'],
         crimson: ['var(--crimson)'],
-        roboto:['var(--roboto)'],
-        robotoMono:['var(--robotoMono)']
+        roboto: ['var(--roboto)'],
+        robotoMono: ['var(--robotoMono)'],
       },
       keyframes: {
         bounce: {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-5px)' },
         },
+        shake: {  // Añadimos la animación shake
+          '0%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+          '75%': { transform: 'translateX(-5px)' },
+          '100%': { transform: 'translateX(0)' },
+        },
       },
       animation: {
         bounce: 'bounce 1s infinite',
+        shake: 'shake 0.5s ease-in-out', // Añadimos la animación shake
       },
     },
   },
