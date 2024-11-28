08be9e9c94b02a3c33f2181d411caa5f5ee1da1b
ale2002solano <ale134valle@gmail.com>
2024-11-28 16:35:28 -0600

    tiempo 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,7 +35,7 @@ const config: Config = {
         },
       },
       animation: {
-        bounce: 'bounce 0.8s infinite',
+        bounce: 'bounce 1s infinite',
         shake: 'shake 0.5s ease-in-out', // Añadimos la animación shake
       },
     },
