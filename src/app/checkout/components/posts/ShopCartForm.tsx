694d8d47d78e9f3bd0ea05fffa0245dfa62e6cb9
ale2002solano <ale134valle@gmail.com>
2024-12-03 09:27:36 -0600

    overflow en div principal

--- src/app/checkout/components/posts/ShopCartForm.tsx
+++ src/app/checkout/components/posts/ShopCartForm.tsx
@@ -398,7 +398,7 @@ useEffect(() => {
 
 
     return (
-        <div className="flex flex-col lg:flex-row justify-between font-koulen w-full p-8 min-h-screen">
+        <div className="flex flex-col lg:flex-row justify-between font-koulen w-full p-8 min-h-screen overflow-y-auto">
             {loading && <LoadingSpinner />}
             <div title="Articulos" className="m-2 rounded-md bg-gray-200 w-full lg:w-1/2 flex-grow p-5 lg:px-10">
             <div id="header" className="text-gray-700 flex flex-row justify-center items-baseline content-stretch flex-wrap">
