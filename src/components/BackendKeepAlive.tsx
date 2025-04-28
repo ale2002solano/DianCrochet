79b3c49cf2e95dc4e9ff089087bcc6018c3d65e0
erickperejil <erickdanielescoto@gmail.com>
2025-04-28 13:05:54 -0600

    funcion innecesaria

--- src/components/BackendKeepAlive.tsx
+++ src/components/BackendKeepAlive.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-// Hook para mantener vivo el backend
-const useKeepAlive = (url: string, interval: number = 30000): void => { 
-    useEffect(() => {
-        const keepAlive = async () => {
-            try {
-                await fetch(url);
-            } catch (error) {
-                console.error("Error manteniendo vivo el backend:", error);
-            }
-        };
+// // Hook para mantener vivo el backend
+// const useKeepAlive = (url: string, interval: number = 30000): void => { 
+//     useEffect(() => {
+//         const keepAlive = async () => {
+//             try {
+//                 await fetch(url);
+//             } catch (error) {
+//                 console.error("Error manteniendo vivo el backend:", error);
+//             }
+//         };
 
-        keepAlive();
-        const intervalId = setInterval(keepAlive, interval); 
+//         keepAlive();
+//         const intervalId = setInterval(keepAlive, interval); 
 
-        return () => clearInterval(intervalId); 
-    }, [url, interval]);
-};
+//         return () => clearInterval(intervalId); 
+//     }, [url, interval]);
+// };
 
-const BackendKeepAlive: React.FC = () => {
-    useKeepAlive("https://vercel-dianas.vercel.app/", 30000);
-    return null;
-};
+// const BackendKeepAlive: React.FC = () => {
+//     useKeepAlive("https://vercel-dianas.vercel.app/", 30000);
+//     return null;
+// };
 
-export default BackendKeepAlive;
+// export default BackendKeepAlive;
